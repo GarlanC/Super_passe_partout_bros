1f0711ffb10b5fc94eb63f2bbaefdd3c9c1a00b7
Alexandre Ostier <alexandre.ostir@etu.univ-smb.fr>
2025-12-12 12:28:41 +0100

    Finition des déplacements

--- Saé/UCForet.xaml.cs
+++ Saé/UCForet.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,42 @@ namespace Saé
     /// </summary>
     public partial class UCForet : UserControl
     {
+        public static string orientationPerso = "Face";
         public UCForet()
         {
             InitializeComponent();
+            string nomFichierImage = $"pack://application:,,,/images/img{MainWindow.Perso}{orientationPerso}.png";
+            imgPerso.Source = new BitmapImage(new Uri(nomFichierImage));
+        }
+
+        public void canvasJeu_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Left && Canvas.GetLeft(imgPerso) <= -25 || e.Key == Key.Right && Canvas.GetLeft(imgPerso) > ActualWidth - imgPerso.Width + 25)
+                Console.WriteLine("Le personnage ne peut pas sortir de la fenêtre");
+            else
+            {
+                if (e.Key == Key.Right)
+                {
+                    Canvas.SetLeft(imgPerso, Canvas.GetLeft(imgPerso) + MainWindow.pasPerso);
+                    imgPerso.Source = new BitmapImage(new Uri($"pack://application:,,,/images/img{MainWindow.Perso}DroiteMarche.png"));
+                }
+                else if (e.Key == Key.Left)
+                {
+                    Canvas.SetLeft(imgPerso, Canvas.GetLeft(imgPerso) - MainWindow.pasPerso);
+                    imgPerso.Source = new BitmapImage(new Uri($"pack://application:,,,/images/img{MainWindow.Perso}GaucheMarche.png"));
+                }
+                else
+                {
+                    imgPerso.Source = new BitmapImage(new Uri($"pack://application:,,,/images/img{MainWindow.Perso}Face.png"));
+                }
+                Console.WriteLine("Position gauche personnage :" + Canvas.GetLeft(imgPerso));
+            }
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.KeyDown += canvasJeu_KeyDown;
         }
     }
 }
+
